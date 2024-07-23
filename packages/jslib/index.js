be7152eb107ae995c67c386a6c74c0d04b0619e7
lijuhong1981 <lijuhong@hotmail.com>
2024-07-23 15:27:34 +0800

    Update index.js

--- packages/jslib/index.js
+++ packages/jslib/index.js
@@ -1,26 +1,28 @@
 import clone from "./src/clone.js";
+import deepAssign from "./src/deepAssign.js";
 import equalsArray from "./src/equalsArray.js";
-import getPropertyDescriptor from "./src/getPropertyDescriptor.js";
 import foreachBigArray from "./src/foreachBigArray.js";
 import Fullscreen from "./src/Fullscreen.js";
+import getPropertyDescriptor from "./src/getPropertyDescriptor.js";
+import HashArray from "./src/HashArray.js";
+import * as logger from "./src/logger.js";
 import merge from "./src/merge.js";
-import deepAssign from "./src/deepAssign.js";
 import NavigatorDetection from "./src/NavigatorDetection.js";
 import setParameters from "./src/setParameters.js";
-import * as logger from "./src/logger.js";
-import HashArray from "./src/HashArray.js";
+import wait from "./src/wait.js";
 
 export {
-    clone,
-    equalsArray,
-    getPropertyDescriptor,
-    foreachBigArray,
     Fullscreen,
-    merge,
-    deepAssign,
-    NavigatorDetection,
-    setParameters,
-    setParameters as setOptions,
-    logger,
     HashArray,
+    NavigatorDetection,
+    clone,
+    deepAssign,
+    equalsArray,
+    foreachBigArray,
+    getPropertyDescriptor,
+    logger,
+    merge,
+    setParameters as setOptions,
+    setParameters,
+    wait
 };
