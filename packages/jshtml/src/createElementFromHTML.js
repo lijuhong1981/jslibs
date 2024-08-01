14d86ebcca26929a87fddba47d40739a5b9780bc
lijuhong1981 <lijuhong@hotmail.com>
2024-08-01 11:32:24 +0800

    Update createElementFromHTML.js

--- packages/jshtml/src/createElementFromHTML.js
+++ packages/jshtml/src/createElementFromHTML.js
@@ -7,7 +7,7 @@ function createElementFromHTML(HTML) {
     const div = document.createElement('div');
     div.innerHTML = HTML;
     // return div.childNodes[0];
-    const domElement = div.childNodes[0];
+    const domElement = div.children[0];
     div.removeChild(domElement);
     return domElement;
 };
