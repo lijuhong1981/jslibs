2c61af6eb08b24b47ccee073642048f44add3d38
lijuhong <lijuhong@pcitech.com>
2025-04-08 15:05:54 +0800

    Task新增Failed状态
    Task新增Failed状态

--- packages/jstask/src/AnimationFrameUpdater.js
+++ packages/jstask/src/AnimationFrameUpdater.js
@@ -3,7 +3,16 @@ import isDefined from "@lijuhong1981/jscheck/src/isDefined.js";
 import Destroyable from "@lijuhong1981/jsdestroy/src/Destroyable.js";
 import now from "@lijuhong1981/jstime/src/now.js";
 
+/**
+ * 动画帧执行器，根据设置的帧率执行动画帧
+ * @see {requestAnimationFrame}
+*/
 class AnimationFrameUpdater extends Destroyable {
+    /**
+     * @constructor
+     * @param {boolean} autoStart 自动开始，默认true
+     * @param {number} framesPerSecond 帧率，默认60
+     */
     constructor(autoStart = true, framesPerSecond = 60) {
         super();
         this.updateFuncs = [];
@@ -17,6 +26,10 @@ class AnimationFrameUpdater extends Destroyable {
             this.start();
     }
 
+    /**
+     * 设置帧率
+     * @param {number} value
+     */
     set framesPerSecond(value) {
         Check.typeOf.number.greaterThan('framesPerSecond', value, 0);
         if (this._framesPerSecond !== value) {
@@ -25,10 +38,17 @@ class AnimationFrameUpdater extends Destroyable {
         }
     }
 
+    /**
+     * 获取帧率
+     * @returns {number}
+     */
     get framesPerSecond() {
         return this._framesPerSecond;
     }
 
+    /**
+     * @private
+    */
     update() {
         if (this.isStoped)
             return this;
@@ -45,6 +65,10 @@ class AnimationFrameUpdater extends Destroyable {
         return this;
     }
 
+    /**
+     * 启动
+     * @returns {this}
+    */
     start() {
         if (this.isStarted)
             return;
@@ -54,6 +78,10 @@ class AnimationFrameUpdater extends Destroyable {
         return this;
     }
 
+    /**
+     * 停止
+     * @returns {this}
+    */
     stop() {
         this.isStoped = true;
         this.isStarted = false;
@@ -64,18 +92,37 @@ class AnimationFrameUpdater extends Destroyable {
         return this;
     }
 
+    /**
+     * 更新函数数量
+     * @returns {number}
+    */
     get numberOfUpdateFuncs() {
         return this.updateFuncs.length;
     }
 
+    /**
+     * 获取一个更新函数索引
+     * @param {Function} updateFunc
+     * @returns {number}
+     */
     indexOf(updateFunc) {
         return this.updateFuncs.indexOf(updateFunc);
     }
 
+    /**
+     * 是否包含一个更新函数
+     * @param {Function} updateFunc
+     * @returns {boolean}
+     */
     contains(updateFunc) {
         return this.indexOf(updateFunc) !== -1;
     }
 
+    /**
+     * 添加一个更新函数
+     * @param {Function} updateFunc
+     * @returns {this}
+     */
     add(updateFunc) {
         Check.typeOf.func('updateFunc', updateFunc);
         if (!this.contains(updateFunc))
@@ -83,6 +130,11 @@ class AnimationFrameUpdater extends Destroyable {
         return this;
     }
 
+    /**
+     * 移除一个更新函数
+     * @param {Function} updateFunc
+     * @returns {this}
+     */
     remove(updateFunc) {
         const index = this.indexOf(updateFunc);
         if (index !== -1)
@@ -90,6 +142,11 @@ class AnimationFrameUpdater extends Destroyable {
         return this;
     }
 
+    /**
+     * 根据索引移除一个更新函数
+     * @param {number} index
+     * @returns {this}
+     */
     removeAt(index) {
         Check.typeOf.integer('index', index);
         const updateFunc = this.updateFuncs[index];
@@ -98,6 +155,10 @@ class AnimationFrameUpdater extends Destroyable {
         return this;
     }
 
+    /**
+     * 移除所有更新函数
+     * @returns {this}
+     */
     removeAll() {
         this.updateFuncs.length = 0;
         return this;
@@ -105,6 +166,7 @@ class AnimationFrameUpdater extends Destroyable {
 
     /**
      * 执行销毁
+     * @private
      */
     onDestroy() {
         this.stop();
@@ -112,22 +174,51 @@ class AnimationFrameUpdater extends Destroyable {
     }
 };
 
-let _instance;
+let _default;
 
 Object.defineProperties(AnimationFrameUpdater, {
+    /**
+     * 获取一个默认实例对象
+    */
+    default: {
+        configurable: false,
+        get: function () {
+            if (!_default)
+                _default = new AnimationFrameUpdater();
+            return _default;
+        }
+    },
+    /**
+     * 获取一个默认实例对象
+    */
+    getDefault: {
+        configurable: false,
+        writable: false,
+        value: function () {
+            return this.default;
+        }
+    },
+    /**
+     * 获取一个默认实例对象
+     * @deprecated
+    */
     instance: {
         configurable: false,
         get: function () {
-            if (!_instance)
-                _instance = new AnimationFrameUpdater();
-            return _instance;
+            console.warn('The instance property has deprecated, use default instead.');
+            return this.default;
         }
     },
+    /**
+     * 获取一个默认实例对象
+     * @deprecated
+    */
     getInstance: {
         configurable: false,
         writable: false,
         value: function () {
-            return this.instance;
+            console.warn('The getInstance function has deprecated, use getDefault instead.');
+            return this.default;
         }
     },
 });
--- packages/jstask/src/TaskPool.js
+++ packages/jstask/src/TaskPool.js
@@ -9,33 +9,64 @@ function getNextTaskId() {
     return TaskId++;
 }
 
+/**
+ * 任务状态
+ * @typedef {Object} TaskState
+ * @property {number} None 任务未开始
+ * @property {number} Executing 任务执行中
+ * @property {number} Finished 任务已结束
+ * @property {number} Canceled 任务已取消
+ * @property {number} Failed 任务失败
+*/
 const TaskState = Object.freeze({
     None: 0,
     Executing: 1,
     Finished: 2,
     Canceled: 3,
+    Failed: 4,
 });
 
 /**
- * 任务对象，需由子类继承实现
+ * 任务对象
 */
 class Task extends Destroyable {
+    /**
+     * @constructor
+     * @param {object} options 配置项
+     * @param {Function} options.onExecute 任务执行函数
+     * @param {Function} options.onFinish 任务完成函数
+     * @param {Function} options.onCancel 任务取消函数
+     * @param {Function} options.onFail 任务失败函数
+     */
     constructor(options = {}) {
         super();
+        /**
+         * 任务id，自动生成
+        */
         this.id = getNextTaskId();
+        /**
+         * 任务状态
+         * @type {TaskState}
+        */
         this.state = TaskState.None;
-        this.priority = 0;
+        // this.priority = 0;
         if (isFunction(options.onExecute))
             this.onExecute = options.onExecute.bind(this);
         if (isFunction(options.onFinish))
             this.onFinish = options.onFinish.bind(this);
         if (isFunction(options.onCancel))
             this.onCancel = options.onCancel.bind(this);
+        if (isFunction(options.onFail))
+            this.onCancel = options.onCancel.bind(this);
     }
 
+    /**
+     * 开始执行任务，由TaskPool自动调用执行
+     * @returns {boolean} 执行结果
+     */
     execute() {
-        if (this.isDestroyed() || this.isFinished || this.isCanceled || this.isExecuting) {
-            console.warn('The id ' + this.id + ' task state is ' + this.state + ', unable do execute.');
+        if (this.isDestroyed() || this.state !== TaskState.None) {
+            console.warn('The id ' + this.id + ' task state is ' + this.state + ', unable to execute.');
             return false;
         }
         this.state = TaskState.Executing;
@@ -43,74 +74,152 @@ class Task extends Destroyable {
         return true;
     }
 
+    /**
+     * 任务执行函数，可由任务初始化时传入或子类实现
+     */
     onExecute() { }
 
+    /**
+     * 任务是否正在执行中
+     * @returns {boolean}
+    */
     get isExecuting() {
         return this.state === TaskState.Executing;
     }
 
+    /**
+     * 取消任务，由外部调用，调用后可取消未执行或执行中的任务
+     * @returns {boolean} 取消结果
+     */
     cancel() {
-        if (this.isDestroyed() || this.isFinished)
+        if (this.isDestroyed() || (this.state !== TaskState.None && this.state !== TaskState.Executing)) {
+            console.warn('The id ' + this.id + ' task state is ' + this.state + ', unable to cancel.');
             return false;
+        }
         this.state = TaskState.Canceled;
         this.onCancel();
         return true;
     }
 
+    /**
+     * 任务取消函数，可由任务初始化时传入或子类实现
+     */
     onCancel() { }
 
+    /**
+     * 任务是否已取消
+     * @returns {boolean}
+    */
     get isCanceled() {
         return this.state === TaskState.Canceled;
     }
 
+    /**
+     * 完成任务，当任务执行完成时调用
+     * @returns {boolean} 执行结果
+     */
     finish() {
-        if (this.isDestroyed())
+        if (this.isDestroyed() || this.state !== TaskState.Executing) {
+            console.warn('The id ' + this.id + ' task state is ' + this.state + ', unable to finish.');
             return false;
+        }
         this.state = TaskState.Finished;
         this.onFinish();
         return true;
     }
 
+    /**
+     * 任务结束函数，可由任务初始化时传入或子类实现
+     */
     onFinish() { }
 
+    /**
+     * 任务是否已结束
+     * @returns {boolean}
+    */
     get isFinished() {
         return this.state === TaskState.Finished;
+    }
+
+    /**
+     * 任务失败，当任务执行失败时调用
+     * @param {*} error 
+     * @returns {boolean} 执行结果
+     */
+    fail(error) {
+        if (this.isDestroyed() || this.state !== TaskState.Executing) {
+            console.warn('The id ' + this.id + ' task state is ' + this.state + ', unable to fail.');
+            return false;
+        }
+        this.state = TaskState.Failed;
+        this.error = error;
+        this.onFail(error);
+        return true;
+    }
+
+    /**
+     * 任务失败函数，可由任务初始化时传入或子类实现
+     * @param {*} error 
+     */
+    onFail(error) { }
+
+    /**
+     * 任务是否失败
+     * @returns {boolean}
+    */
+    get isFailed() {
+        return this.state === TaskState.Failed;
     }
 };
 
 /**
- * 任务池，用于管理任务的执行与销毁
+ * 任务池，用于管理任务的执行与销毁。
+ * 
+ * 与线程池类似，可根据设置的可执行任务数自动执行放入任务池中的任务。
  */
 class TaskPool extends Destroyable {
     constructor(maximumNumber = 5) {
         super();
-        //任务同时执行的最大数量
+        /**
+         * 任务同时执行的最大数量
+         * @type {number}
+        */
         this.maximumNumber = maximumNumber;
-        //任务队列
+        /**
+         * 任务队列
+         * @type {Array<Task>}
+        */
         this.taskQueue = [];
 
         this.update = this.update.bind(this);
-        // this.update();
-        AnimationFrameUpdater.instance.add(this.update);
+        AnimationFrameUpdater.default.add(this.update);
     }
 
-    push(task) {
-        Check.instanceOf('task', task, Task);
-        this.taskQueue.push(task);
+    /**
+     * 添加任务
+     * @param {Array<Task>} tasks
+     * @returns {this}
+     */
+    push(...tasks) {
+        tasks.forEach(task => {
+            Check.instanceOf('task', task, Task);
+            this.taskQueue.push(task);
+        });
+        return this;
     }
 
+    /**
+     * @private
+    */
     update() {
         if (this.isDestroyed())
             return;
-        this.onUpdate();
-    }
 
-    onUpdate() {
         let executingCount = 0;
         const unexecuteTasks = [];
         const tasks = this.taskQueue.slice();
         tasks.forEach(task => {
-            if (task.isFinished || task.isCanceled) { //移除掉已完成或已取消的任务
+            if (task.isFinished || task.isCanceled || task.isFailed) { //移除掉已完成或已取消或已失败的任务
                 const index = this.taskQueue.indexOf(task);
                 if (index !== -1)
                     this.taskQueue.splice(index, 1);
@@ -133,24 +242,61 @@ class TaskPool extends Destroyable {
 
     /**
      * 执行销毁
+     * @private
      */
     onDestroy() {
-        AnimationFrameUpdater.instance.remove(this.update);
+        AnimationFrameUpdater.default.remove(this.update);
         this.taskQueue.length = 0;
     }
 };
 
-let _instance;
+let _default;
 
 Object.defineProperties(TaskPool, {
+    /**
+     * 获取一个默认实例对象
+    */
+    default: {
+        configurable: false,
+        get: function () {
+            if (!_default)
+                _default = new AnimationFrameUpdater();
+            return _default;
+        }
+    },
+    /**
+     * 获取一个默认实例对象
+    */
+    getDefault: {
+        configurable: false,
+        writable: false,
+        value: function () {
+            return this.default;
+        }
+    },
+    /**
+     * 获取一个默认实例对象
+     * @deprecated
+    */
     instance: {
         configurable: false,
         get: function () {
-            if (!_instance)
-                _instance = new TaskPool();
-            return _instance;
+            console.warn('The instance property has deprecated, use default instead.');
+            return this.default;
         }
-    }
+    },
+    /**
+     * 获取一个默认实例对象
+     * @deprecated
+    */
+    getInstance: {
+        configurable: false,
+        writable: false,
+        value: function () {
+            console.warn('The getInstance function has deprecated, use getDefault instead.');
+            return this.default;
+        }
+    },
 });
 
 export {
