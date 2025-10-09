cd25c05da4ad10c4d974af8398a20a374956076e
lijuhong <lijuhong@pcitech.com>
2025-10-09 14:49:32 +0800

    Update getPosition.js

--- packages/jshtml/src/getPosition.js
+++ packages/jshtml/src/getPosition.js
@@ -1,12 +1,18 @@
 /**
  * 计算DOMEvent真实的Position
- * @param {Event} event DOMEvent对象
+ * @param {MouseEvent} event DOMEvent对象
  * @param {HTMLElement} domElement DOM对象，可不填，不填时使用event.currentTarget或event.target
- * @param {Object} result 计算坐标结果对象，可不填
- * @returns {Object} 返回计算坐标结果对象
+ * @param {object} result 计算坐标结果对象，可不填
+ * @returns {{x:number, y:number}} 返回计算坐标结果对象
  */
 function getPosition(event, domElement, result = {}) {
-    domElement = domElement || event.currentTarget || event.target;
+    domElement = domElement || event.target || event.currentTarget;
+    if (domElement === document) {
+        // pageX / pageY needed for iOS
+        result.x = event.clientX ?? event.pageX;
+        result.y = event.clientY ?? event.pageY;
+        return result;
+    }
     const bounds = domElement.getBoundingClientRect();
     // pageX / pageY needed for iOS
     result.x = event.clientX ?? event.pageX - bounds.left;
