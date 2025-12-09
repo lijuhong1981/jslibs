04dd7d48ce6415f036a604e2080522d78ba945ac
lijuhong <lijuhong@pcitech.com>
2025-12-09 11:05:46 +0800

    Update extractBaseName.js

--- packages/jsurl/src/extractBaseName.js
+++ packages/jsurl/src/extractBaseName.js
@@ -1,21 +1,16 @@
+import extractFileName from "./extractFileName.js";
+
 /**
  * 从url中提取不包含扩展名的文件名
  * @param {String} url 传入的url
  * @returns {String} 提取的文件名
  */
 function extractBaseName(url) {
-    let index = url.lastIndexOf('?');
-    if (index !== -1)
-        url = url.substring(0, index);
-
-    index = url.lastIndexOf('.');
-    if (index !== -1)
-        url = url.substring(0, index);
-
-    index = url.lastIndexOf('/');
+    const fileName = extractFileName(url);
+    const index = fileName.lastIndexOf('.');
     if (index === -1)
-        return url;
-    return url.substring(index + 1);
+        return fileName;
+    return fileName.substring(0, index);
 };
 
 export default extractBaseName;
