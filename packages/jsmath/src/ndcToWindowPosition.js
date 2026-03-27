97b9518618bc32146fcb5a13d82b63fef29b2257
lijuhong <lijuhong@pcitech.com>
2026-03-27 11:39:21 +0800

    Update ndcToWindowPosition.js

--- packages/jsmath/src/ndcToWindowPosition.js
+++ packages/jsmath/src/ndcToWindowPosition.js
@@ -3,11 +3,11 @@
  * @param {Object} ndc 需转换的ndc坐标, {x: 0.5, y: -0.5, z: 0.0}
  * @param {Number} width 窗口像素宽
  * @param {Number} height 窗口像素高
- * @param {Object} result 输出结果对象
  * @param {Boolean} checkInvisible 是否检查不可见，默认true
+ * @param {Object} result 输出结果对象
  * @returns {Object|undefined} 输出结果对象, 如：{x: 123, y: 321}，返回undefined表示不可见
  */
-function ndcToWindowPosition(ndc, width, height, result = {}, checkInvisible = true) {
+function ndcToWindowPosition(ndc, width, height, checkInvisible = true, result = {}) {
 
     // invisible
     if (checkInvisible && (ndc.x < -1 || ndc.x > 1 || ndc.y < -1 || ndc.y > 1 || ndc.z < 0 || ndc.z > 1))
